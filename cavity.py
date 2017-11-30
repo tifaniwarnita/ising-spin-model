0fbf1e1708ba11af16c8bd1a54c2f17f0ded29e9
Tifani Warnita <tifani@ks.c.titech.ac.jp>
2017-11-30 09:59:05 +0900

    Update cavity codes and update figures

--- cavity.py
+++ cavity.py
@@ -24,6 +24,8 @@ def create_spins(n_spin):
 # The nearest interactions are handled exactly
 def spin_avg_cavity(spins, b, J, h):
 	spin_avg = spins
+	
+	# forward
 	tetha1to12 = b * h
 	tetha12to2 = np.arctanh(np.tanh(tetha1to12) * np.tanh(b * J))
 	tetha2to23 = b * h + tetha12to2
@@ -31,17 +33,29 @@ def spin_avg_cavity(spins, b, J, h):
 	tetha3to34 = b * h + tetha23to3
 	tetha34to4 = np.arctanh(np.tanh(tetha3to34) * np.tanh(b * J))
 	tetha4to45 = b * h + tetha34to4
+	tetha45to5 = np.arctanh(np.tanh(tetha4to45) * np.tanh(b * J))
 
-	spin_avg[0] = np.tanh(b * h + tetha12to)
-	spin_avg[1] = np.tanh(b * (J * spin_avg[0] + J * spin_avg[2] + h))
-	spin_avg[2] = np.tanh(b * (J * spin_avg[1] + J * spin_avg[3] + h))
-	spin_avg[3] = np.tanh(b * (J * spin_avg[2] + J * spin_avg[4] + h))
-	spin_avg[4] = np.tanh(b * (J * spin_avg[3] + h))
+	# backward
+	tetha5to45 = b * h
+	tetha45to4 = np.arctanh(np.tanh(tetha5to45) * np.tanh(b * J))
+	tetha4to34 = b * h + tetha45to4
+	tetha34to3 = np.arctanh(np.tanh(tetha4to34) * np.tanh(b * J))
+	tetha3to23 = b * h + tetha34to3
+	tetha23to2 = np.arctanh(np.tanh(tetha3to23) * np.tanh(b * J))
+	tetha2to12 = b * h + tetha23to2
+	tetha12to1 = np.arctanh(np.tanh(tetha2to12) * np.tanh(b * J))
+
+	spin_avg[0] = np.tanh(b * h + tetha12to1)
+	spin_avg[1] = np.tanh(b * h + tetha12to2 + tetha23to2)
+	spin_avg[2] = np.tanh(b * h + tetha23to3 + tetha34to3)
+	spin_avg[3] = np.tanh(b * h + tetha34to4 + tetha45to4)
+	spin_avg[4] = np.tanh(b * h + tetha45to5)
 	return spin_avg
 
 def main():
 	spins = create_spins(VAL_n_spin)
 	filename = "MFA_" + str(spins) + '.png'
+	x_label = ['s1', 's2', 's3', 's4', 's5']
 	description = "[s1, s2, s3, s4, s5] = " + str(spins)
 
 	s1 = []
@@ -50,24 +64,13 @@ def main():
 	s4 = []
 	s5 = []
 	
-	for i in np.arange(VAL_iteration):
-		spin_avg = spin_avg_cavity(spins, VAL_b, VAL_J, VAL_h)
-		s1.append(spin_avg[0])
-		s2.append(spin_avg[1])
-		s3.append(spin_avg[2])
-		s4.append(spin_avg[3])
-		s5.append(spin_avg[4])
-
-	line_s1 = plt.plot(np.arange(VAL_iteration), s1, label='<s1>')
-	line_s2 = plt.plot(np.arange(VAL_iteration), s2, label='<s2>')
-	line_s3 = plt.plot(np.arange(VAL_iteration), s3, label='<s3>')
-	line_s4 = plt.plot(np.arange(VAL_iteration), s4, label='<s4>')
-	line_s5 = plt.plot(np.arange(VAL_iteration), s5, label='<s5>')
-
+	spin_avg = spin_avg_cavity(spins, VAL_b, VAL_J, VAL_h)
+	line_s1 = plt.plot(x_label, spin_avg)
+	
 	plt.legend(loc='lower right', shadow=True)
 	plt.gca().set_position((.13, .3, .8, .6))
 
-	description += "\n\nResult when the spin averages are convergent:"
+	description += "\n\nResult:"
 	description += "\n<s1> = " + str(spin_avg[0])
 	description += ", <s2> = " + str(spin_avg[1])
 	description += ", <s3> = " + str(spin_avg[2])
@@ -76,7 +79,7 @@ def main():
 	plt.figtext(.02, .02, description)
 
 	plt.title('Cavity Method')
-	plt.xlabel("Iteration")
+	plt.xlabel("Spin")
 	plt.ylabel("Spin Average")
 	plt.grid()
 
